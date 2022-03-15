68faa9ec492f3f177742fab92ab96756b37eac30
Carlos Ricardo Villena Cabrejos <cvillenacabrejos@gmail.com>
2022-03-15 16:26:30 -0500

    Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,19 @@
+package ProblemasArrays;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int a[]= new int [2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums.length; j++){
+                int sum=nums[i]+nums[j];
+                if(sum==target && i!=j){
+                    a[0]=i;
+                    a[1]=j;
+                    return a;
+                }
+            }
+        }
+        return a;
+
+    }
+}
